604d9be7ce05261711a190a40b80ad55c8afef56
Jamaal Locke <bamojam@gmail.com>
2013-05-24 21:54:05 -0500

    Creates initial framework that shows window and registers event handlers

---
+++ Reflexes For Friends/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Reflexes_For_Friends
+{
+    static class Program
+    {
+        static void Main()
+        {
+            RenderWindow window = new RenderWindow(new VideoMode(800, 800), "Reflexes For Friends", Styles.Default, new ContextSettings(32, 0));
+            window.SetVerticalSyncEnabled(true);
+
+            #region Register Event Handlers
+            window.Closed += new EventHandler(OnClosed);
+            window.KeyPressed += new EventHandler<KeyEventArgs>(OnKeyPressed);
+            #endregion
+
+            window.SetActive();
+            while (window.IsOpen())
+            {
+                window.DispatchEvents();
+
+                window.Clear(Color.Cyan);
+
+                window.Display();
+            }
+        }
+
+        static void OnClosed(object sender, EventArgs e)
+        {
+            Window window = (Window)sender;
+            window.Close();
+        }
+
+        static void OnKeyPressed(object sender, KeyEventArgs e)
+        {
+        }
+    }
+}
